643c911840b6db1480a4617b774f6a0b3ff5f979
ansza <137867309+ansza@users.noreply.github.com>
2023-09-29 16:52:51 +0000

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
+
 
 def get_reviews(url):
     # Send a GET request to the app URL
@@ -32,41 +31,4 @@ st.title('Google Play Store Review Extractor')
 
 # Get the app URL from the user
 url = st.text_input('Enter the URL of the app in the Google Play Store:', help="Example: https://play.google.com/store/apps/details?id=com.google.android.apps.maps")
-
-# If the user has entered a URL, extract the reviews and display them
-if url:
-    st.write(f'Extracting reviews from {url}...')
-    reviews = get_reviews(url)
-    st.write(f'Found {len(reviews)} reviews:')
-    
-    # Create a list of review texts and labels
-    review_texts = []
-    review_labels = []
-    for review in reviews:
-        st.write(f"Author: {review['author']}")
-        st.write(f"Date: {review['date']}")
-        st.write(f"Rating: {review['rating']}")
-        st.write(f"Comment: {review['comment']}")
-        st.write('---')
-        review_texts.append(review['comment'])
-        if review['rating'] < 3:
-            review_labels.append('negative')
-        else:
-            review_labels.append('positive')
-    
-    # Train a text classification model
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(review_texts)
-    y = review_labels
-    clf = MultinomialNB()
-    clf.fit(X, y)
-    
-    # Get a text input from the user and predict its sentiment
-    text_input = st.text_input('Enter a review text to predict its sentiment:')
-    if text_input:
-        X_test = vectorizer.transform([text_input])
-        y_pred = clf.predict(X_test)
-        st.write(f"Predicted sentiment: {y_pred[0]}")
-else:
-    st.write("Please enter the URL of the app in the Google Play Store above.")
 
